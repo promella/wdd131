4cbc05962fe6a0943588c0f4b8189fef864cd41a
promellawdd130 <pncube8@byupathway.edu>
2026-03-06 14:13:59 +0200

    index html

---
+++ scripts/getdate.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear
+
+document.getElementById("lastModified").innerHTML = Last Modification:
